4afddcffc4d822cbb44b6c3e30f7cf896e5c91fc
Derril Lucci <dlucci@derrils-mbp.lan>
2023-01-06 07:19:14 -0500

    testing something...

--- app/src/main/java/com/example/newsly/ui/MainActivity.kt
+++ app/src/main/java/com/example/newsly/ui/MainActivity.kt
@@ -1,10 +1,11 @@
 package com.example.newsly.ui
 
 import android.content.Intent
+import android.graphics.*
+import android.graphics.drawable.Drawable
 import android.net.Uri
 import android.os.Bundle
 import androidx.activity.compose.setContent
-import com.example.newsly.viewmodel.MainViewModel
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.background
@@ -19,9 +20,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
+import androidx.palette.graphics.Palette
 import coil.compose.AsyncImage
 import com.example.newsly.model.Multimedia
 import com.example.newsly.model.TopStories
+import com.example.newsly.viewmodel.MainViewModel
 
 
 class MainActivity : AppCompatActivity() {
@@ -61,6 +64,7 @@ class MainActivity : AppCompatActivity() {
     fun ArticleRow(story: TopStories) {
 
         val multimedia = story.multimedia?.first()
+        var drawable :Drawable? = null
         Box(
             modifier = Modifier
                 .clickable(
@@ -81,23 +85,42 @@ class MainActivity : AppCompatActivity() {
             AsyncImage(
                 model = multimedia?.url,
                 contentDescription = null,
-                modifier = Modifier.fillMaxWidth()
-            )
-            Text(
-                text = story.title ?: "",
-                fontFamily = NYTFont,
-                fontSize = 20.sp,
-                color = Color.White,
-                modifier = Modifier.align(Alignment.TopStart)
+                modifier = Modifier.fillMaxWidth(),
+                onSuccess = {
+                    drawable = it.result.drawable
+                    createTitle()
+                }
             )
             Text(
                 text = story.byline ?: "",
                 fontFamily = NYTFont,
-                color = Color.White,
+                color = getAverageOffset(drawable),
                 fontSize = 15.sp,
                 modifier = Modifier.align(Alignment.BottomEnd)
             )
         }
+    }
+
+    @Composable
+    fun createTitle() {
+        Text(
+            text = story.title ?: "",
+            fontFamily = NYTFont,
+            fontSize = 20.sp,
+            ≠color = Color.White,
+            modifier = Modifier.align(Alignment.TopStart)
+        )
+    }
+
+    private fun getAverageOffset(drawable: Drawable?) : Color {
+        val bitmap = Bitmap.createBitmap(drawable?.intrinsicWidth ?: 0, drawable?.intrinsicHeight ?: 0, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(bitmap)
+        drawable?.setBounds(0, 0, drawable.intrinsicWidth, drawable.intrinsicHeight)
+
+        drawable?.draw(canvas)
+
+        val palette = Palette.from(bitmap).generate()
+        return Color(palette.dominantSwatch?.rgb ?: 0)
     }
 
     @Preview
@@ -105,7 +128,7 @@ class MainActivity : AppCompatActivity() {
     fun PreviewRow() {
 
         val multimedia = Multimedia(
-            url = "https://www.nytimes.com/live/2022/03/31/business/economy-news-opec-inflation"
+            url = "https://static01.nyt.com/images/2022/06/14/world/14buffalo-shooting-neighborhood-segregation/merlin_206869314_d31e5711-af35-41c6-9aaf-452897b9a5c5-superJumbo.jpg"
         )
 
         val topStories = TopStories(
