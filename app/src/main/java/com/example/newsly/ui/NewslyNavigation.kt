908e05951e77e600fa106d868398fa68aac4302c
Derril Lucci <derril@mac.lan>
2025-03-15 07:29:23 -0400

    Adding a fade animation when an article is clicked

--- app/src/main/java/com/example/newsly/ui/NewslyNavigation.kt
+++ app/src/main/java/com/example/newsly/ui/NewslyNavigation.kt
@@ -1,13 +1,20 @@
 package com.example.newsly.ui
 
+import android.annotation.SuppressLint
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
+import androidx.compose.animation.togetherWith
 import androidx.compose.runtime.Composable
-
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 
 import androidx.navigation.compose.rememberNavController
 
+@SuppressLint("UnusedContentLambdaTargetStateParameter")
 @Composable
 fun NewslyNavigation(navController: NavHostController = rememberNavController()) {
     NavHost(navController = navController, startDestination = Routes.Home.route) {
@@ -16,7 +23,16 @@ fun NewslyNavigation(navController: NavHostController = rememberNavController())
         }
         composable(Routes.Article.route + "/{url}") {
             val url = it.arguments?.getString("url")
-            StoryViewer(navController = navController, url = url ?: "")
+            AnimatedContent(
+                targetState = it.arguments?.getString("url"),
+                transitionSpec = {
+                    slideInVertically { it } + fadeIn() togetherWith
+                            slideOutVertically { -it } + fadeOut()
+                }
+            ) {
+                StoryViewer(navController = navController, url = url ?: "")
+            }
+
         }
     }
 }
@@ -24,5 +40,4 @@ fun NewslyNavigation(navController: NavHostController = rememberNavController())
 sealed class Routes(val route: String) {
     object Home : Routes("home")
     object Article : Routes("article")
-
 }
